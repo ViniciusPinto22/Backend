570a258703a4754f0198b88f2e0a5c50e093f82b
ViniciusPinto22 <vinicius.pinto.senai404@gmail.com>
2024-08-02 10:46:12 -0300

    Php

---
+++ variaves.php
@@ -0,0 +1,14 @@
+<?php
+
+$nome = 'Senai ';
+$numero = 428;
+$cidade = 'cerquilho ';
+$curso =  'Desenvolvimento de Sistema';
+$media_final = 9,5;
+
+echo $nome;
+echo $numero;
+echo $cidade;
+echo $curso;
+echo $media_final;
+
